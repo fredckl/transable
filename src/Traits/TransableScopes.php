db1aca225e3b4b4bd19386a161f5687c83d03982
Frederic KOLLER <koller.f@gmail.com>
2019-08-12 10:43:45 +0200

    Add where query for translations

--- src/Traits/TransableScopes.php
+++ src/Traits/TransableScopes.php
@@ -11,23 +11,59 @@ use Illuminate\Support\Facades\App;
 trait TransableScopes
 {
 
-    public function scopeWhereTranslation (Builder $query, $translationField, $value = null, ?string $locale = null, string $method = 'whereHas', string $operator = '=')
+    public function scopeWhereTranslation (Builder $query, $column, $operator = null, $value = null, ?string $locale = null, $method = "whereHas")
     {
-        return $query->$method('translations', function (Builder $query) use ($translationField, $value, $locale, $operator) {
-            if (is_array($translationField)) {
-                $conditions = $translationField;
-            } else {
-                $conditions = [
-                    'field' => $translationField,
+        $operators = $query->getQuery()->operators;
+        if (!in_array(\strtolower($operator), $operators)) {
+            $method = $locale ?? $method;
+            $locale = $value;
+            $value = $operator;
+            $operator = "=";
+        }
+
+        return $query->$method('translations', function (Builder $query) use ($column, $operator, $value, $locale) {
+
+            if (!is_array($column)) {
+                $column = [
+                    'field' => $column,
                     ['content', $operator, $value]
                 ];
             }
-            $query->where($conditions);
+            $query->where($column);
+
             if ($locale) {
                 $query->where('locale', '=', $locale);
             }
         });
     }
+//    public function scopeWhereTranslation__ (Builder $query, $translationField, $value = null, ?string $locale = null, string $method = 'whereHas', string $operator = '=')
+//    {
+//        return $query->$method('translations', function (Builder $query) use ($translationField, $value, $locale, $operator) {
+//            if (is_array($translationField)) {
+//                $conditions = $translationField;
+//            } else {
+//                $conditions = [
+//                    'field' => $translationField,
+//                    ['content', $operator, $value]
+//                ];
+//            }
+//            $query->where($conditions);
+//
+//            if ($locale) {
+//                $query->where('locale', '=', $locale);
+//            }
+//        });
+//    }
+
+    public function scopeOrWhereTranslation(Builder $query, string $column, $operator, $value, ?string $locale = null)
+    {
+        return $this->scopeWhereTranslation($query, $column, $operator, $value, $locale, 'orWhereHas');
+    }
+    public function scopeOrWhereTranslationLike(Builder $query, string $column,  $value, ?string $locale = null)
+    {
+        return $this->scopeWhereTranslation($query, $column, 'like', $value, $locale, 'orWhereHas');
+    }
+
 
     public function scopeTranslatedIn(Builder $query, ?string $locale = null)
     {
